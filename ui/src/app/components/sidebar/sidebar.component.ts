17f87798c3f79957d1ec4dae6516f812fa254c12
Hari Prasad <haribrothers@gmail.com>
2025-02-09 01:33:03 +0530

    change label

--- ui/src/app/components/sidebar/sidebar.component.ts
+++ ui/src/app/components/sidebar/sidebar.component.ts
@@ -21,16 +21,16 @@ import { CommonModule } from '@angular/common';
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
         </svg>
-        <span class="sidebar-tooltip group-hover:scale-100">Partials</span>
+        <span class="sidebar-tooltip group-hover:scale-100">Components</span>
       </div>
 
       <div class="sidebar-icon group" routerLink="/variables" routerLinkActive="text-white bg-primary-600">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
         </svg>
-        <span class="sidebar-tooltip group-hover:scale-100">Variables</span>
+        <span class="sidebar-tooltip group-hover:scale-100">Fields</span>
       </div>
     </div>
   `
 })
-export class SidebarComponent {}
+export class SidebarComponent { }
